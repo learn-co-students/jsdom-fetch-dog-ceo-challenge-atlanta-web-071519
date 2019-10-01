75692f01548f9014581d0f7bb077edcd7ef06bd7
brad-bartlett <bradbartlett222@gmail.com>
2019-09-30 22:48:45 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -12,24 +12,8 @@ function fetchPics() {
     fetch(IMGURL)
     .then(resp => resp.json())
     .then(pics => {
-        console.log(pics)
-        renderPics(pics)
+        pics.message.forEach(pic => addPic(pic))
 
     })
 }
 
-
-function renderPics(pics) {
-    pics.forEach(pic => {
-        renderPic(pic)
-    }
-    )
-}
-
-function renderPic(pic) {
-    const singlePic = document.createElement('li')
-    const dogContainer = document.getElementById('dog-image-container')
-    singlePic.dataset.id = pic.id
-    dogContainer.appendChild(singlePic)
-    
-}
